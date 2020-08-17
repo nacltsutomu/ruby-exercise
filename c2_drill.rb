6b458f9be5e49697b72f3c0d93396b6ecb79c5cf
tsutomu kadowaki <tsutomu.kadowaki@nacl-med.co.jp>
2020-08-17 09:27:13 +0900

    練習問題Ｃ２，Ｃ３

---
+++ c2_drill.rb
@@ -0,0 +1,15 @@
+puts 2 + 3
+puts 2 * 2 * 3.14
+puts "Ruby"
+puts "abc" + "def"
+puts "123".to_i + "456".to_i
+a = 400
+b = 100
+puts "コーヒー：#{a}円"
+puts "合計：#{a + b * 2}円"
+total = 300 * 2
+p total
+tax = total * 0.08
+puts tax
+total = 300 * 2
+puts total
---
+++ c3_drill.rb
@@ -0,0 +1,65 @@
+a = 2
+puts a < 365
+
+a = 2
+puts a == 1 + 1
+
+season = "春"
+unless season == "夏"
+  puts "あんまんたべたい"
+end
+season = "春"
+if season != "夏"
+  puts "あんまんたべたい！"
+end
+
+season = "夏"
+if season == "夏"
+  puts "かき氷たべたい"
+  puts "麦茶のみたい"
+end
+
+wallet = 100
+if wallet >= 120
+  puts "ジュースを買おう"
+else
+  puts "お金じゃ買えない幸せがたくさんあるんだ"
+end
+
+x = 200
+if x <= 0 || x >= 100
+  puts "範囲外です"
+end
+
+x = 0
+y = -1
+z = 1
+if x > 0 || y > 0 || z > 0
+  puts "正の数です"
+end
+
+season = "春"
+case season
+when "春"
+  puts "アイスを買っていこう"
+when "夏"
+  puts "かき氷買ってこう！"
+else
+  puts "あんまん買ってこう！"
+end
+
+2.times do
+  puts "カフェラテ"
+  2.times do
+    puts "モカ"
+  end
+end
+puts "フラペチーノ"
+
+2.times {
+    puts "カフェラテ！"
+    2.times {
+      puts "モカ！"
+    }
+}
+puts "フラペチーノ！"
